286e51d2a17d85c630e072cc9e753250e7fa0c4c
Chilop <94477621+Chilop@users.noreply.github.com>
2022-10-24 08:43:29 -0500

    Aimbot partial fixed (#7)
    * Aimbot partial fixed
    
    * player aimbot fixed and partial main menu created
    
    * Update Main Menu.unity
    
    * Aimbot fixed and fuctional

--- Proyecto2PS/Assets/_Main/Scripts/PlayerAimBot.cs
+++ Proyecto2PS/Assets/_Main/Scripts/PlayerAimBot.cs
@@ -1,17 +1,58 @@
 
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerAimBot : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> _enemyList;
+    [SerializeField] private int _aimRange = default;
+    private GameObject _objectToFollow = default;
+    private CircleCollider2D _circleCollider2D = default;
     private Rigidbody2D _playerRigidbody2D = null;
-    private List<GameObject> _enemylist = new List<GameObject>(0);
-    private SphereCollider _sphereCollider = default;
-    private GameObject _enemy;
+    private void Start()
+    {
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
+        _circleCollider2D = GetComponent<CircleCollider2D>();
+        _circleCollider2D.radius = _aimRange;
+    }
 
     void Update()
     {
-        Vector2 lookdirection = _enemy.transform.position - transform.position;
+        if (_enemyList.Count == 0) return;
+        
+        LookEnemy();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy")) return;
+
+        _enemyList.Add(collision.gameObject);
+
+        if (_enemyList.Count != 1) return;
+
+        Retarget();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Enemy")) return;
+
+        _enemyList.Remove(collision.gameObject);
+
+        Retarget();
+    }
+
+    private void Retarget()
+    {
+        _objectToFollow = _enemyList.FirstOrDefault();
+    }   
+
+    private void LookEnemy()
+    {
+        Vector2 lookdirection = _enemyList.FirstOrDefault().transform.position - transform.position;
         float angle = Mathf.Atan2(lookdirection.y, lookdirection.x) * Mathf.Rad2Deg - 90f;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         _playerRigidbody2D.transform.rotation = rotation;
