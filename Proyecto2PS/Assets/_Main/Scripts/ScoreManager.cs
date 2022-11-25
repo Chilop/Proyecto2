521ab2383334e0a6f19ce0393127776142c67e39
Chilop <zap520@amerike.edu.mx>
2022-11-25 02:13:47 -0600

    Main Menu improved

--- Proyecto2PS/Assets/_Main/Scripts/ScoreManager.cs
+++ Proyecto2PS/Assets/_Main/Scripts/ScoreManager.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
-    private Text _scoreText;
-    private int _score = default;
+    private Text _scoreText = null;
+    private Text _highScoreText = null;
     private void Awake()
     {
         if(Instance != null)
